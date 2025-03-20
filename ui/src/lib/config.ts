6e7315a7a5fbcd6caec95b590e3b0756666c4a09
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-03-20 18:21:59 +0300

    Add public development API base and auth URLs to configuration

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -10,7 +10,7 @@ const config: Config = {
 
 if (!config.apiBaseUrl) {
     if (import.meta.env.DEV) {
-        config.apiBaseUrl = 'http://localhost:5204';
+        config.apiBaseUrl = import.meta.env.PUBLIC_DEV_API_BASE_URL;
     } else {
         throw new Error('Missing PUBLIC_API_BASE_URL');
     }
@@ -18,7 +18,7 @@ if (!config.apiBaseUrl) {
 
 if (!config.apiAuthUrl) {
     if (import.meta.env.DEV) {
-        config.apiAuthUrl = 'http://localhost:5203';
+        config.apiAuthUrl = import.meta.env.PUBLIC_DEV_API_AUTH_URL;
     } else {
         throw new Error('Missing PUBLIC_API_AUTH_URL');
     }
