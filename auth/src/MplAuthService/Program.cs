dfa779a03bc60ab45507f9d52210f4e5dd587349
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-03-20 13:43:45 +0300

    Update CORS configuration to use allowed origins from settings and restrict methods to GET, POST, and DELETE

--- auth/src/MplAuthService/Program.cs
+++ auth/src/MplAuthService/Program.cs
@@ -91,10 +91,10 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(builder =>
     {
         builder
-            .WithOrigins("http://127.0.0.1:5173", "http://localhost:5173")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials();
+            .WithOrigins(configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? ["http://127.0.0.1:5173", "http://localhost:5173"])
+                .WithMethods("GET", "POST", "DELETE")
+                .AllowAnyHeader()
+                .AllowCredentials();
     });
 });
 
