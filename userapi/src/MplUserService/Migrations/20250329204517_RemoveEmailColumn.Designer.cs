b1825e7c904fb624be7e3c7ee01de4abfcc350a7
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-03-29 23:47:22 +0300

    Remove Email property from User model and associated database migration for PII protection. Fixes #115

--- userapi/src/MplUserService/Migrations/20250329204517_RemoveEmailColumn.Designer.cs
+++ userapi/src/MplUserService/Migrations/20250329204517_RemoveEmailColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MplUserService.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MplUserService.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20250329204517_RemoveEmailColumn")]
+    partial class RemoveEmailColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- userapi/src/MplUserService/Models/User.cs
+++ userapi/src/MplUserService/Models/User.cs
@@ -3,7 +3,6 @@ namespace MplUserService.Models
     public class User
     {
         public string Id { get; set; } = null!;
-        public string Email { get; set; } = null!;
         public List<int> FavouriteIds { get; set; } = [];
     }
 }
