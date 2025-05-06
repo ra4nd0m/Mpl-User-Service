ccbb26e64d181997b7f9b975e6380fc9e70ccb4c
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-05-06 15:13:41 +0300

    refactor: sort favoriteIds and add conditional rendering for empty state

--- ui/src/routes/dashboard/workdesk/pricetracking/+page.svelte
+++ ui/src/routes/dashboard/workdesk/pricetracking/+page.svelte
@@ -2,11 +2,13 @@
 	import { favoritesStore } from '$lib/stores/favouritesStore';
 	import PriceTable from './components/PriceTable.svelte';
 
-	const favoriteIds = $derived($favoritesStore.ids);
-
+	const favoriteIds = $derived($favoritesStore.ids.sort((a, b) => a - b));
 </script>
 
-{#each favoriteIds as id}
-	<PriceTable materialId={id} />
-{/each}
-
+{#if favoriteIds.length === 0}
+	<p>No favorite materials found.</p>
+{:else}
+	{#each favoriteIds as id}
+		<PriceTable materialId={id} />
+	{/each}
+{/if}
