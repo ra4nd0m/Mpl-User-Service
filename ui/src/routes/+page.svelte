642d8c7d53d73f9a4ee2098108ce9e4fd72b4859
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-06-02 23:07:33 +0300

    refactor: Remove unused import of refreshAccessToken from authClient

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import { onMount } from 'svelte';
 	import { authStore } from '$lib/stores/authStore';
 	import { goto } from '$app/navigation';
-	import { refreshAccessToken } from '$lib/api/authClient';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
