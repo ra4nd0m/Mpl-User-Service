684c338cf823162ab3fffefe4f9a42febce0a8c6
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-06-04 21:27:40 +0300

    feat: remove Filters DbSet and related model configuration from UserContext

--- userapi/src/MplUserService/Data/UserContext.cs
+++ userapi/src/MplUserService/Data/UserContext.cs
@@ -10,8 +10,6 @@ namespace MplUserService.Data
         }
 
         public DbSet<User> Users { get; set; } = null!;
-        public DbSet<Filter> Filters { get; set; } = null!;
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -19,21 +17,6 @@ namespace MplUserService.Data
             {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.FavouriteIds)
-                    .HasColumnType("jsonb");
-            });
-
-            modelBuilder.Entity<Filter>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Groups)
-                    .HasColumnType("jsonb");
-                entity.Property(e => e.Sources)
-                    .HasColumnType("jsonb");
-                entity.Property(e => e.Units)
-                    .HasColumnType("jsonb");
-                entity.Property(e => e.MaterialIds)
-                    .HasColumnType("jsonb");
-                entity.Property(e => e.Properties)
                     .HasColumnType("jsonb");
             });
         }
