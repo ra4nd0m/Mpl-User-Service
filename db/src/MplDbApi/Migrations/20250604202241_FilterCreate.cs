c99795e1ec2ba78523e94d4f4507dfbaedaa0172
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-06-04 23:24:07 +0300

    feat: add Filter migration and update Entity Framework Core packages

---
+++ db/src/MplDbApi/Migrations/20250604202241_FilterCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MplDbApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FilterCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AffectedRole = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Groups = table.Column<string>(type: "TEXT", nullable: true),
+                    Sources = table.Column<string>(type: "TEXT", nullable: true),
+                    Units = table.Column<string>(type: "TEXT", nullable: true),
+                    MaterialIds = table.Column<string>(type: "TEXT", nullable: true),
+                    Properties = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filters", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filters");
+        }
+    }
+}
