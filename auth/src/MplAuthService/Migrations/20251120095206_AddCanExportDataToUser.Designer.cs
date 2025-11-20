4c4d402a2598c00b6686acb32778691e80aae876
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-11-20 12:52:34 +0300

    feat: add CanExportData property to User model and update migrations

--- auth/src/MplAuthService/Migrations/20251120095206_AddCanExportDataToUser.Designer.cs
+++ auth/src/MplAuthService/Migrations/20251120095206_AddCanExportDataToUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MplAuthService.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MplAuthService.Migrations
 {
     [DbContext(typeof(AuthContext))]
-    partial class AuthContextModelSnapshot : ModelSnapshot
+    [Migration("20251120095206_AddCanExportDataToUser")]
+    partial class AddCanExportDataToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
