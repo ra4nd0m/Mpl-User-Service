d3eb8c7ed5fe3ae14225995bdef3d1992f4a899a
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2026-02-20 07:44:07 +0300

    feat: enhance report file handling with download and cancel functionality

--- ui/src/routes/dashboard/reports/+page.svelte
+++ ui/src/routes/dashboard/reports/+page.svelte
@@ -3,15 +3,38 @@
 	import { authStore } from '$lib/stores/authStore';
 	import { onMount } from 'svelte';
 	import AddFileModal from './AddFileModal.svelte';
-	import { getFilesList, type UserFileMetadata } from '$lib/api/fileClient';
+	import {
+		getFilesList,
+		type UserFile,
+		type DownloadStatus,
+		type UserFileMetadata,
+		downloadFile
+	} from '$lib/api/fileClient';
 
 	const isAdmin = $derived($authStore.roles?.includes('Admin'));
 	let showAddFileModal = $state(false);
 
 	let reportList = $state<UserFileMetadata[]>([]);
+	let reportFilesList = $derived<UserFile[]>(
+		reportList.map((item) => ({
+			...item,
+			status: 'pending' as DownloadStatus,
+			abortController: null as AbortController | null
+		}))
+	);
 
 	function openAddFileModal() {
 		showAddFileModal = true;
+	}
+
+	function handleDownload(file: UserFile) {
+		downloadFile(file);
+	}
+
+	function handleCancelDownload(file: UserFile) {
+		if (file.abortController) {
+			file.abortController.abort();
+		}
 	}
 
 	onMount(async () => {
@@ -47,17 +70,21 @@
 	{#if isAdmin}
 		<button onclick={openAddFileModal}>Open add file modal</button>
 	{/if}
-	
+
 	<h2>Reports</h2>
-	{#if reportList.length === 0}
+	{#if reportFilesList.length === 0}
 		<p>No reports available</p>
 	{:else}
 		<ul>
-			{#each reportList as file}
+			{#each reportFilesList as file}
 				<li>
 					<strong>{file.fileName}</strong>
 					<span>{new Date(file.uploadedAt).toLocaleDateString()}</span>
-					<button>Download</button>
+					{#if file.status === 'pending' || file.status === 'cancelled' || file.status === 'error'}
+						<button onclick={() => handleDownload(file)}>Download</button>
+					{:else if file.status === 'downloading'}
+						<button onclick={() => handleCancelDownload(file)}>Cancel Download</button>
+					{/if}
 				</li>
 			{/each}
 		</ul>
