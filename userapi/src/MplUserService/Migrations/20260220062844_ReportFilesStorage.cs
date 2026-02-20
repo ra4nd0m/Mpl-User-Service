90cd2bcddb1ddb142b359569bd15e1074b5a8f87
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2026-02-20 09:30:31 +0300

    feat: add ReportFiles migration and update UserContext model snapshot

---
+++ userapi/src/MplUserService/Migrations/20260220062844_ReportFilesStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MplUserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReportFilesStorage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportFiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    StoredName = table.Column<string>(type: "text", nullable: false),
+                    RequiredSubscription = table.Column<int>(type: "integer", nullable: false),
+                    UploadedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportFiles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportFiles");
+        }
+    }
+}
